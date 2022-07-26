114d77c6f7a9f4d223d9009520cb64f03e1b9c24
Bitna Kim <boboroo1211@gmail.com>
2022-07-26 17:04:22 +0900

    Added library Hilt.

--- app/src/main/java/com/shiny/bookapp/MyApplication.kt
+++ app/src/main/java/com/shiny/bookapp/MyApplication.kt
@@ -3,7 +3,9 @@ package com.shiny.bookapp
 import android.app.Application
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MyApplication: Application() {
     init {
         context = this
